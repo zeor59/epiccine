58bf729b2d280ce1fa7d033e1e618e7cd67e5d72
Mickael Blondeau <mickael.blondeau59@gmail.com>
2014-02-27 14:13:49 +0100

    Edit menu

--- app/controllers/admin/movie_characters_controller.rb
+++ app/controllers/admin/movie_characters_controller.rb
@@ -1,14 +1,14 @@
 #encoding: utf-8
 
-class MovieCharactersController < ApplicationController
+class Admin::MovieCharactersController < AdminController
 
     def create
         movie_character = MovieCharacter.new(movie_params)
 
         if movie_character.save
-            redirect_to edit_movie_path(movie_params['movie_id']), notice: 'Personne ajoutée au film !'
+            redirect_to edit_admin_movie_path(movie_params['movie_id']), notice: 'Personne ajoutée au film !'
         else    
-            redirect_to edit_movie_path(movie_params['movie_id']), alert: 'Erreur lors de l\'ajout.'
+            redirect_to edit_admin_movie_path(movie_params['movie_id']), alert: 'Erreur lors de l\'ajout.'
         end
     end
 
@@ -16,7 +16,7 @@ class MovieCharactersController < ApplicationController
         movie_character = MovieCharacter.find(params[:id])
         movie_id = movie_character.movie_id
         movie_character.destroy
-        redirect_to movie_path(movie_id)
+        redirect_to admin_movie_path(movie_id)
     end
     
     def movie_params
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ EpicCine::Application.routes.draw do
   namespace 'admin' do
     resources :movies
     resources :characters
+    resources :movie_characters
     root 'movies#index'
   end
 
