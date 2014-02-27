98b33119963e192c10c3130acd7a8ff7d41ede59
Mickael Blondeau <mickael.blondeau59@gmail.com>
2014-02-27 15:48:38 +0100

    Amelioration page Char

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,9 @@
 class Character < ActiveRecord::Base
 	validates :first_name, presence: true
-	has_many :moviecharacters
-	has_many :movies, through: :moviecharacters
+	validates :last_name, presence: true
+	validates :biography, presence: true
+	validates :picture, presence: true
+	validates :birthday, presence: true
+	has_many :movie_characters
+	has_many :movies, through: :movie_characters
 end
